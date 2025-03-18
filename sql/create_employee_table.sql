5741f33468ef054599a59f01e68dbe6f4807d543
Yiming Pu <pymmmmmm@qq.com>
2025-03-18 11:57:36 +0800

    修改员工表建表sql

--- sql/create_employee_table.sql
+++ sql/create_employee_table.sql
@@ -5,7 +5,8 @@ DROP TABLE IF EXISTS mdm_employee;
 
 CREATE TABLE mdm_employee
 (
-    employee_code VARCHAR(6) PRIMARY KEY COMMENT '员工编码',
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '组织ID',
+    employee_code VARCHAR(6)  NOT NULL UNIQUE COMMENT '员工编码',
     employee_name VARCHAR(50) NOT NULL COMMENT '员工姓名',
     create_by     VARCHAR(64)  DEFAULT NULL COMMENT '创建者',
     create_time   DATETIME     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -19,8 +20,8 @@ CREATE TABLE mdm_employee
 -- 员工表初始化数据
 -- ----------------------------
 INSERT INTO mdm_employee
-VALUES ('600001', '黄裕辉', 'admin', NOW(), NULL, NULL, NULL);
+VALUES (NULL, '600001', '黄裕辉', 'admin', NOW(), NULL, NULL, NULL);
 INSERT INTO mdm_employee
-VALUES ('600002', '卫国', 'admin', NOW(), NULL, NULL, NULL);
+VALUES (NULL, '600002', '卫国', 'admin', NOW(), NULL, NULL, NULL);
 INSERT INTO mdm_employee
-VALUES ('600003', '孔岩', 'admin', NOW(), NULL, NULL, NULL);
+VALUES (NULL, '600003', '孔岩', 'admin', NOW(), NULL, NULL, NULL);

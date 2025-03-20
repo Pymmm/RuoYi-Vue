cb990ab0c0b5b2eeff512c425185d17ca9f5f2ff
Yiming Pu <pymmmmmm@qq.com>
2025-03-20 14:45:49 +0800

    引入easyexcel和api文档备注

--- ruoyi-mdm/src/main/java/com/ruoyi/mdm/domain/Department.java
+++ ruoyi-mdm/src/main/java/com/ruoyi/mdm/domain/Department.java
@@ -1,5 +1,12 @@
 package com.ruoyi.mdm.domain;
 
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.HeadFontStyle;
+import com.alibaba.excel.annotation.write.style.HeadRowHeight;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
@@ -11,39 +18,59 @@ import com.ruoyi.common.core.domain.BaseEntity;
  * @author Yiming Pu
  * @date 2025-03-20
  */
+@ExcelIgnoreUnannotated // 在导出Excel时，忽略没有被任何注解标注的字段
+@ColumnWidth(16) // 设置列的宽度
+@HeadRowHeight(14) // 设置表头行的高度
+@HeadFontStyle(fontHeightInPoints = 11) // 设置表头字体样式
+@ApiModel(description = "部门管理对象")
 public class Department extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /** 部门ID */
+    @ApiModelProperty(value = "部门ID")
     private Long id;
 
     /** 部门编码 */
     @Excel(name = "部门编码")
+    @ExcelProperty("组部门码")
+    @ApiModelProperty(value = "部门编码")
     private String departmentCode;
 
     /** 组织编码 */
     @Excel(name = "组织编码")
+    @ExcelProperty("组织编码")
+    @ApiModelProperty(value = "组织编码")
     private String organizationCode;
 
     /** 组织名称 */
     @Excel(name = "组织名称")
+    @ExcelProperty("组织名称")
+    @ApiModelProperty(value = "组织名称")
     private String organizationName;
 
     /** 一级编码 */
     @Excel(name = "一级编码")
+    @ExcelProperty("一级编码")
+    @ApiModelProperty(value = "一级编码")
     private String unitCode;
 
     /** 一级单位 */
     @Excel(name = "一级单位")
+    @ExcelProperty("一级单位")
+    @ApiModelProperty(value = "一级单位")
     private String unitName;
 
     /** 二级编码 */
     @Excel(name = "二级编码")
+    @ExcelProperty("二级编码")
+    @ApiModelProperty(value = "二级编码")
     private String subUnitCode;
 
     /** 二级单位 */
     @Excel(name = "二级单位")
+    @ExcelProperty("二级单位")
+    @ApiModelProperty(value = "二级单位")
     private String subUnitName;
 
     public void setId(Long id)
